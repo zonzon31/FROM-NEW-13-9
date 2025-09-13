9b92dcbf906014a19ae88604ff7c6850ec847704
zonzon31 <zonzo177@gmail.com>
2025-09-13 13:24:25 +0700

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,5 +1,5 @@
 const config = {
-    token: '8086831835:AAGp7bTGis_7WFTrDtIrzZI03_SOt-bUj2w',
+    token: '8391941092:AAFJ0AT19r7epHH0Zo91Obzyj133RsHxFVs',
     chat_id: '1922578871',
     noti_token: '',
     noti_chat_id: ''
